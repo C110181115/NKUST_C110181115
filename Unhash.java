dcffbf5fa7547d077474c39c0be7c4d91c24c9cd
cloud1001 <95233508+C110181115@users.noreply.github.com>
2021-11-30 01:21:30 +0800

    Add files via upload

--- Unhash.java
+++ Unhash.java
@@ -7,17 +7,17 @@ class UnHash{
 
     public static String letters = "cdefghijlmnoqstuvxz";
 
-    public static String revHash(long hash) {
+    public static String revHash(long hash) {                            //數字轉換成英文
         String result="";
-        String newResult="";
+        String newResult="";                                             //空字串
 
-        while(hash != 7){
-            result = result + letters.charAt((int) (hash % 23));
+        while(hash != 7){                                                //當條件hash不等於7 繼續跑迴圈
+            result = result + letters.charAt((int) (hash % 23));         //"abcde" charAt(3)=d
             hash = hash/ 23;
         }
 
-        for (int i = result.length(); i>=1; i--){
-            newResult += result.charAt(i-1);
+        for (int i = result.length(); i>=1; i--){                        //ilovecoding是倒著顯示
+            newResult = newResult + result.charAt(i-1);
         }
         return newResult;
     }
@@ -25,7 +25,7 @@ class UnHash{
     public static long hash(String s){
         long h = 7;
         for (int i = 0; i < s.length(); i++){
-            h = h * 23 + letters.indexOf(s.charAt(i));
+            h = h * 23 + letters.indexOf(s.charAt(i));                   //"abcde" indexOf("b")=1
         }
         return h;
     }
