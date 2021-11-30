906a73389ce31305604a34ba5ec17a455e270365
cloud1001 <95233508+C110181115@users.noreply.github.com>
2021-12-01 01:33:49 +0800

    Add files via upload

---
+++ spider.java
@@ -0,0 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class spider {
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("https://www.ptt.cc/bbs/Kaohsiung/M.1638247579.A.E82.html").get();
+            System.out.println("doc.title()"  + doc.title());
+
+            Element link = doc.select("link").first();
+            String absHref = link.attr("href");
+            System.out.println(absHref);
+
+            Elements newsHeadlines = doc.select("div.push");
+            for (Element headline : newsHeadlines) {
+                System.out.println(headline.text());
+                break;
+            }    //QQ
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
